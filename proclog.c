813e7dd4eb869d689c1c6c1bde4c97d6c9dab188
Jachian <author.tle@gmail.com>
2023-04-04 12:21:33 -0400

    test new function

--- proclog.c
+++ proclog.c
@@ -113,7 +113,7 @@ static int endflag;
  * This function is called then the /proc file is read
  *
  */
-int procfile_read(char *buffer,
+static ssize_t procfile_read(char *buffer,
 	      char **buffer_location,
 	      off_t offset, int buffer_length, int *eof, void *data)
 {
@@ -140,7 +140,7 @@ int procfile_read(char *buffer,
 // }
 
 // function to write to proc file
-int procfile_write(struct file *file, const char *buffer, size_t count, loff_t *off)
+static ssize_t procfile_write(struct file *file, const char *buffer, size_t count, loff_t *off)
 {
 	// get buffer size 
 	procfs_buffer_size = count;
