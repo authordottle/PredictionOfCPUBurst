df722ab5e49b43a84cb569dd3d40cbfa9a0926b1
Jachian <author.tle@gmail.com>
2023-04-08 17:47:54 -0400

    test file

--- proclog.c
+++ proclog.c
@@ -1,197 +1,237 @@
-// Logger that creates a proc file
-// idea from tldp.org/LDP/lkmpg/2.6/html/index.html
-#include <linux/version.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/init.h>
-#include <linux/proc_fs.h>
-#include <linux/seq_file.h> // seq_read, ...
-#include <linux/uaccess.h>
+#include <linux/syscalls.h>
+#include <linux/sched.h>
 
-#ifndef __KERNEL__
-#define __KERNEL__
-#endif
+asmlinkage long (*original_sys_fork)(struct pt_regs *regs);
 
+asmlinkage long new_sys_fork(struct pt_regs *regs)
+{
+    // Log process activity information, such as process IDs and user IDs
+    printk(KERN_INFO "Fork: Parent %d forked child %d\n", current->pid, task->pid);
+    
+    // Call the original system call
+    return original_sys_fork(regs);
+}
+
+static int __init process_logger_init(void)
+{
+    // Hook into the fork system call
+    write_cr0(read_cr0() & (~0x10000));
+    original_sys_fork = (void *)sys_call_table[__NR_fork];
+    sys_call_table[__NR_fork] = new_sys_fork;
+    write_cr0(read_cr0() | 0x10000);
+
+    return 0;
+}
+
+static void __exit process_logger_exit(void)
+{
+    // Restore the original fork system call
+    write_cr0(read_cr0() & (~0x10000));
+    sys_call_table[__NR_fork] = original_sys_fork;
+    write_cr0(read_cr0() | 0x10000);
+}
+
+module_init(process_logger_init);
+module_exit(process_logger_exit);
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Kernel module to log process times");
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 6, 0)
-#define HAVE_PROC_OPS
-#endif
 
-// size of buffer ~32Kb
-#define PROCFS_MAX_SIZE 32768
+// // Logger that creates a proc file
+// // idea from tldp.org/LDP/lkmpg/2.6/html/index.html
+// #include <linux/version.h>
+// #include <linux/module.h>
+// #include <linux/kernel.h>
+// #include <linux/init.h>
+// #include <linux/proc_fs.h>
+// #include <linux/seq_file.h> // seq_read, ...
+// #include <linux/uaccess.h>
 
-// buffer to hold information from log
-static char procfs_buffer[PROCFS_MAX_SIZE];
+// #ifndef __KERNEL__
+// #define __KERNEL__
+// #endif
 
-// size of buffer
-static unsigned long procfs_buffer_size = 0;
+// MODULE_LICENSE("GPL");
+// MODULE_DESCRIPTION("Kernel module to log process times");
 
-// pointer for buffer location in read
-static char *buff_ptr;
+// #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 6, 0)
+// #define HAVE_PROC_OPS
+// #endif
 
-// struct to hold info about proc file
-struct proc_dir_entry *log_file;
+// // size of buffer ~32Kb
+// #define PROCFS_MAX_SIZE 32768
 
-static int endflag;
+// // buffer to hold information from log
+// static char procfs_buffer[PROCFS_MAX_SIZE];
 
-static void *proc_seq_start(struct seq_file *s, loff_t *pos)
-{
+// // size of buffer
+// static unsigned long procfs_buffer_size = 0;
 
-	printk("Start of sequence read!\n");
+// // pointer for buffer location in read
+// static char *buff_ptr;
 
-	(*pos) = endflag;
+// // struct to hold info about proc file
+// struct proc_dir_entry *log_file;
 
-	buff_ptr = procfs_buffer + ((*pos) * sizeof(char));
+// static int endflag;
 
-	// if pos is greater than or equal to buffer size then leave sequence read
-	if ((*pos) >= procfs_buffer_size - 1 || *buff_ptr == '\0')
-	{
-		printk("End sequence read\n");
-		return NULL;
-	}
+// static void *proc_seq_start(struct seq_file *s, loff_t *pos)
+// {
 
-	printk("Place in buffer is: %Ld\n", (*pos));
+// 	printk("Start of sequence read!\n");
 
-	return buff_ptr;
-}
+// 	(*pos) = endflag;
 
-static void *proc_seq_next(struct seq_file *s, void *v, loff_t *pos)
-{
-	printk("Sequence Next!");
-	char *temp = (char *)v;
-	while ((*temp) != '\n')
-	{
+// 	buff_ptr = procfs_buffer + ((*pos) * sizeof(char));
 
-		(*pos)++;
-		printk("position increased");
-		if ((*pos) >= procfs_buffer_size)
-		{
-			return NULL;
-		}
-		temp++;
-		printk("temp increased");
-	}
-	temp++;
-	endflag = (*pos);
-	printk("position is %Ld\n", (*pos));
-	return temp;
-}
+// 	// if pos is greater than or equal to buffer size then leave sequence read
+// 	if ((*pos) >= procfs_buffer_size - 1 || *buff_ptr == '\0')
+// 	{
+// 		printk("End sequence read\n");
+// 		return NULL;
+// 	}
 
-static void proc_seq_stop(struct seq_file *s, void *v)
-{
-	printk("Sequence stop!");
-	buff_ptr = NULL;
-	printk("Sequence stop 2: electric bugaloo");
-}
+// 	printk("Place in buffer is: %Ld\n", (*pos));
 
-static int proc_seq_show(struct seq_file *s, void *v)
-{
-	printk("Showing value");
-	char *temp = (char *)v;
-	do
-	{
-		seq_putc(s, *temp);
-		temp++;
-	} while (*temp != '\n');
-	seq_putc(s, '\n');
-	return 0;
-}
+// 	return buff_ptr;
+// }
 
-static struct seq_operations proc_seq_ops = {
-	.start = proc_seq_start,
-	.next = proc_seq_next,
-	.stop = proc_seq_stop,
-	.show = proc_seq_show
-	};
+// static void *proc_seq_next(struct seq_file *s, void *v, loff_t *pos)
+// {
+// 	printk("Sequence Next!");
+// 	char *temp = (char *)v;
+// 	while ((*temp) != '\n')
+// 	{
 
-static int procfile_open(struct inode *inode, struct file *file)
-{
-	printk("open procfile");
-	return seq_open(file, &proc_seq_ops);
-}
+// 		(*pos)++;
+// 		printk("position increased");
+// 		if ((*pos) >= procfs_buffer_size)
+// 		{
+// 			return NULL;
+// 		}
+// 		temp++;
+// 		printk("temp increased");
+// 	}
+// 	temp++;
+// 	endflag = (*pos);
+// 	printk("position is %Ld\n", (*pos));
+// 	return temp;
+// }
 
-// function to write to proc file
-static ssize_t procfile_write(struct file *file, const char *buffer, size_t count, loff_t *off)
-{
-	// set buffer size
-	procfs_buffer_size += count;
-	if (procfs_buffer_size > PROCFS_MAX_SIZE)
-	{
-		procfs_buffer_size = PROCFS_MAX_SIZE;
-		printk("Proc file buffer overflow");
-	}
-	else
-	{
-		printk("Buffer size updated to: %lu", procfs_buffer_size);
-	}
+// static void proc_seq_stop(struct seq_file *s, void *v)
+// {
+// 	printk("Sequence stop!");
+// 	buff_ptr = NULL;
+// 	printk("Sequence stop 2: electric bugaloo");
+// }
 
-	// write data to buffer
-	printk("Process Info: %s", (procfs_buffer + (procfs_buffer_size - count)));
+// static int proc_seq_show(struct seq_file *s, void *v)
+// {
+// 	printk("Showing value");
+// 	char *temp = (char *)v;
+// 	do
+// 	{
+// 		seq_putc(s, *temp);
+// 		temp++;
+// 	} while (*temp != '\n');
+// 	seq_putc(s, '\n');
+// 	return 0;
+// }
 
-	if (copy_from_user(procfs_buffer + (procfs_buffer_size - count), buffer, count))
-	{
-		return -EFAULT;
-	}
+// static struct seq_operations proc_seq_ops = {
+// 	.start = proc_seq_start,
+// 	.next = proc_seq_next,
+// 	.stop = proc_seq_stop,
+// 	.show = proc_seq_show
+// 	};
 
-	return count;
-}
+// static int procfile_open(struct inode *inode, struct file *file)
+// {
+// 	printk("open procfile");
+// 	return seq_open(file, &proc_seq_ops);
+// }
 
-// struct that holds what functions run for different aspects of log file
-#ifdef HAVE_PROC_OPS
-static const struct proc_ops log_file_fops = {
-	.proc_open = procfile_open,
-	.proc_write = procfile_write,
-	.proc_read = seq_read,
-	.proc_lseek = seq_lseek,
-	.proc_release = seq_release
-	};
-#else
-static const struct file_operations log_file_fops = {
-	.owner = THIS_MODULE,
-	.open = procfile_open,
-	.read = seq_read,
-	.write = procfile_write,
-	.llseek = seq_lseek,
-	.release = seq_release
-	};
-#endif
+// // function to write to proc file
+// static ssize_t procfile_write(struct file *file, const char *buffer, size_t count, loff_t *off)
+// {
+// 	// set buffer size
+// 	procfs_buffer_size += count;
+// 	if (procfs_buffer_size > PROCFS_MAX_SIZE)
+// 	{
+// 		procfs_buffer_size = PROCFS_MAX_SIZE;
+// 		printk("Proc file buffer overflow");
+// 	}
+// 	else
+// 	{
+// 		printk("Buffer size updated to: %lu", procfs_buffer_size);
+// 	}
 
-static void log_processes(void)
-{
-	struct task_struct *task;
-	for_each_process(task)
-	{
-		printk(KERN_INFO "Process: %s (pid: %d)\n", task->comm, task->pid);
-	}
-}
+// 	// write data to buffer
+// 	printk("Process Info: %s", (procfs_buffer + (procfs_buffer_size - count)));
 
-static int __init init_module(void)
-{	
-	// adapted from stackoverflow.com/questions/8516021/proc-create-example-for-kernel-module
-	// fixed the version issue from https://stackoverflow.com/questions/64931555/how-to-fix-error-passing-argument-4-of-proc-create-from-incompatible-pointer
-	log_file = proc_create("timing_log", 0, NULL, &proc_file_fops);
-	if (log_file == NULL)
-	{
-		return -ENOMEM;
-	}
+// 	if (copy_from_user(procfs_buffer + (procfs_buffer_size - count), buffer, count))
+// 	{
+// 		return -EFAULT;
+// 	}
 
-	endflag = 0;
+// 	return count;
+// }
 
-	printk(KERN_INFO "Process logger module loaded\n");
+// // struct that holds what functions run for different aspects of log file
+// #ifdef HAVE_PROC_OPS
+// static const struct proc_ops log_file_fops = {
+// 	.proc_open = procfile_open,
+// 	.proc_write = procfile_write,
+// 	.proc_read = seq_read,
+// 	.proc_lseek = seq_lseek,
+// 	.proc_release = seq_release
+// 	};
+// #else
+// static const struct file_operations log_file_fops = {
+// 	.owner = THIS_MODULE,
+// 	.open = procfile_open,
+// 	.read = seq_read,
+// 	.write = procfile_write,
+// 	.llseek = seq_lseek,
+// 	.release = seq_release
+// 	};
+// #endif
 
-	// log_processes();
+// static void log_processes(void)
+// {
+// 	struct task_struct *task;
+// 	for_each_process(task)
+// 	{
+// 		printk(KERN_INFO "Process: %s (pid: %d)\n", task->comm, task->pid);
+// 	}
+// }
 
-	return 0;
-}
+// static int __init init_module(void)
+// {	
+// 	// adapted from stackoverflow.com/questions/8516021/proc-create-example-for-kernel-module
+// 	// fixed the version issue from https://stackoverflow.com/questions/64931555/how-to-fix-error-passing-argument-4-of-proc-create-from-incompatible-pointer
+// 	log_file = proc_create("timing_log", 0, NULL, &proc_file_fops);
+// 	if (log_file == NULL)
+// 	{
+// 		return -ENOMEM;
+// 	}
 
-static void __exit exit_module(void)
-{
-	remove_proc_entry("timing_log", NULL);
-	printk(KERN_INFO "Process logger module unloaded\n");
-	return;
-}
+// 	endflag = 0;
 
-module_init(init_module);
-module_exit(exit_module);
+// 	printk(KERN_INFO "Process logger module loaded\n");
+
+// 	// log_processes();
+
+// 	return 0;
+// }
+
+// static void __exit exit_module(void)
+// {
+// 	remove_proc_entry("timing_log", NULL);
+// 	printk(KERN_INFO "Process logger module unloaded\n");
+// 	return;
+// }
+
+// module_init(init_module);
+// module_exit(exit_module);
