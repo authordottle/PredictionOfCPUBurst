7b71b07037d1631e388de96b389b19863c77e0f2
Jachian <author.tle@gmail.com>
2023-04-08 19:00:24 -0400

    test file

--- proclog.c
+++ proclog.c
@@ -36,81 +36,84 @@ struct proc_dir_entry *log_file;
 
 static int endflag;
 
-// static void *proc_seq_start(struct seq_file *s, loff_t *pos)
-// {
+static void *proc_seq_start(struct seq_file *s, loff_t *pos)
+{
+	printk("Hit proc_seq_start");
 
-// 	printk("Start of sequence read!\n");
+	// (*pos) = endflag;
 
-// 	(*pos) = endflag;
+	// buff_ptr = procfs_buffer + ((*pos) * sizeof(char));
 
-// 	buff_ptr = procfs_buffer + ((*pos) * sizeof(char));
+	// // if pos is greater than or equal to buffer size then leave sequence read
+	// if ((*pos) >= procfs_buffer_size - 1 || *buff_ptr == '\0')
+	// {
+	// 	printk("End sequence read\n");
+	// 	return NULL;
+	// }
 
-// 	// if pos is greater than or equal to buffer size then leave sequence read
-// 	if ((*pos) >= procfs_buffer_size - 1 || *buff_ptr == '\0')
-// 	{
-// 		printk("End sequence read\n");
-// 		return NULL;
-// 	}
+	// printk("Place in buffer is: %Ld\n", (*pos));
 
-// 	printk("Place in buffer is: %Ld\n", (*pos));
+	// return buff_ptr;
+	return null;
+}
 
-// 	return buff_ptr;
-// }
+static void *proc_seq_next(struct seq_file *s, void *v, loff_t *pos)
+{
+	printk("Hit proc_seq_next");
+	// printk("Sequence Next!");
+	// char *temp = (char *)v;
+	// while ((*temp) != '\n')
+	// {
 
-// static void *proc_seq_next(struct seq_file *s, void *v, loff_t *pos)
-// {
-// 	printk("Sequence Next!");
-// 	char *temp = (char *)v;
-// 	while ((*temp) != '\n')
-// 	{
+	// 	(*pos)++;
+	// 	printk("position increased");
+	// 	if ((*pos) >= procfs_buffer_size)
+	// 	{
+	// 		return NULL;
+	// 	}
+	// 	temp++;
+	// 	printk("temp increased");
+	// }
+	// temp++;
+	// endflag = (*pos);
+	// printk("position is %Ld\n", (*pos));
+	// return temp;
+	return null;
+}
 
-// 		(*pos)++;
-// 		printk("position increased");
-// 		if ((*pos) >= procfs_buffer_size)
-// 		{
-// 			return NULL;
-// 		}
-// 		temp++;
-// 		printk("temp increased");
-// 	}
-// 	temp++;
-// 	endflag = (*pos);
-// 	printk("position is %Ld\n", (*pos));
-// 	return temp;
-// }
+static void proc_seq_stop(struct seq_file *s, void *v)
+{
+	// printk("Sequence stop!");
+	// buff_ptr = NULL;
+	// printk("Sequence stop 2: electric bugaloo");
+	printk("Hit proc_seq_stop");
+}
 
-// static void proc_seq_stop(struct seq_file *s, void *v)
-// {
-// 	printk("Sequence stop!");
-// 	buff_ptr = NULL;
-// 	printk("Sequence stop 2: electric bugaloo");
-// }
+static int proc_seq_show(struct seq_file *s, void *v)
+{
+	// printk("Showing value");
+	// char *temp = (char *)v;
+	// do
+	// {
+	// 	seq_putc(s, *temp);
+	// 	temp++;
+	// } while (*temp != '\n');
+	// seq_putc(s, '\n');
+	printk("Hit proc_seq_show");
+	return 0;
+}
 
-// static int proc_seq_show(struct seq_file *s, void *v)
-// {
-// 	printk("Showing value");
-// 	char *temp = (char *)v;
-// 	do
-// 	{
-// 		seq_putc(s, *temp);
-// 		temp++;
-// 	} while (*temp != '\n');
-// 	seq_putc(s, '\n');
-// 	return 0;
-// }
-
-// static struct seq_operations proc_seq_ops = {
-// 	.start = proc_seq_start,
-// 	.next = proc_seq_next,
-// 	.stop = proc_seq_stop,
-// 	.show = proc_seq_show
-// 	};
+static struct seq_operations proc_seq_ops = {
+	.start = proc_seq_start,
+	.next = proc_seq_next,
+	.stop = proc_seq_stop,
+	.show = proc_seq_show
+};
 
 static int procfile_open(struct inode *inode, struct file *file)
 {
 	printk("Hit procfile_open");
-	return 0;
-	// return seq_open(file, &proc_seq_ops);
+	return seq_open(file, &proc_seq_ops);
 }
 
 // function to write to proc file
@@ -153,7 +156,7 @@ static const struct proc_ops proc_file_fops = {
 static const struct file_operations proc_file_fops = {
 	.owner = THIS_MODULE,
 	.open = procfile_open,
-	.write = procfile_write,
+	// .write = procfile_write,
 	.read = seq_read,
 	.llseek = seq_lseek,
 	.release = seq_release
@@ -184,8 +187,8 @@ static int __init init_kernel_module(void)
 		return -ENOMEM;
 	}
 
-	// loop processes
-	log_processes();
+	// // loop processes
+	// log_processes();
 
 	return 0;
 }
