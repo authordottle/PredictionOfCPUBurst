4d624522e47b7ad357a3f81e353d15d4bd85a1ef
Jachian <author.tle@gmail.com>
2023-04-09 02:17:51 -0400

    test file

--- proclog.c
+++ proclog.c
@@ -59,10 +59,10 @@ static void proc_seq_stop(struct seq_file *s, void *v)
 }
 
 /* Function to get CPU usage for a process */
-static long get_process_cpu_usage(struct task_struct *task)
+static unsigned long get_process_cpu_usage(struct task_struct *task)
 {
-	long utime, stime, total_time;
-	long long starttime;
+	unsigned long utime, stime, total_time;
+	unsigned long long starttime;
 
 	utime = task->utime;
 	stime = task->stime;
@@ -70,14 +70,13 @@ static long get_process_cpu_usage(struct task_struct *task)
 
 	total_time = utime + stime;
 
-	// /* Calculate the elapsed time since the process started */
-	// unsigned long long elapsed = (ktime_get_ns() - starttime) / NSEC_PER_SEC;
+	/* Calculate the elapsed time since the process started */
+	unsigned long long elapsed = (ktime_get_ns() - starttime) / NSEC_PER_SEC;
 
-	// /* Calculate CPU usage as a percentage */
-	// unsigned long cpu_usage = total_time * 100 / elapsed;
+	/* Calculate CPU usage as a percentage */
+	unsigned long cpu_usage = total_time * 100 / elapsed;
 
-// test
-	return total_time;
+	return cpu_usage;
 }
 
 static int proc_seq_show(struct seq_file *s, void *v)
@@ -94,10 +93,10 @@ static int proc_seq_show(struct seq_file *s, void *v)
 		printk(KERN_INFO "Process: %s (pid: %d)\n", task->comm, task->pid);
 
 		/* Get CPU usage for the process */
-		long cpu_usage = get_process_cpu_usage(task);
+		unsigned long cpu_usage = get_process_cpu_usage(task);
 
 		seq_printf(s,
-				   "%d\t %s\t %ld\t %d\t %p\t %p\t\n ",
+				   "%d\t %s\t %lu\t %d\t %p\t %p\t\n ",
 				   task->pid,
 				   task->comm,
 				   cpu_usage,
