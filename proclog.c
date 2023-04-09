4c6dbec1ce445c52e485e94e28ac5c1592edbace
Jachian <author.tle@gmail.com>
2023-04-09 17:49:23 -0400

    test

--- proclog.c
+++ proclog.c
@@ -87,7 +87,7 @@ static long get_process_cpu_usage(struct task_struct *task)
 	usage_sec = utime_sec + stime_sec;
 	cpu_usage = usage_sec * 100 / elapsed_sec;
 
-	return task->utime;
+	return task->stime;
 }
 
 static int proc_seq_show(struct seq_file *s, void *v)
