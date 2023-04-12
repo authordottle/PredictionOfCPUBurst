827e1c1c028ba7c096d9f87ade78704163aeb9ea
Jachian <author.tle@gmail.com>
2023-04-12 13:52:30 -0400

    test

--- data_collecting/index.c
+++ data_collecting/index.c
@@ -16,11 +16,9 @@
 int complete_time;
 FILE *fp, *outfp;
 
-// Note: This function returns a pointer to a substring of the original string.
-// If the given string was allocated dynamically, the caller must not overwrite
-// that pointer with the returned value, since the original pointer must be
-// deallocated using the same allocator with which it was allocated.  The return
-// value must NOT be deallocated using free() etc.
+// If the given string was allocated dynamically, the caller must not overwrite that pointer with the returned value
+// since the original pointer must be deallocated using the same allocator with which it was allocated.
+// Return a pointer to a substring of the original string. The return value must NOT be deallocated using free() etc.
 char *trim_white_space(char *str)
 {
     char *end;
