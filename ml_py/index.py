23bcc011249485d140b8394e9e480d1b0f5f72ff
Jachian <author.tle@gmail.com>
2023-04-12 22:12:10 -0400

    use ms

--- ml_py/index.py
+++ ml_py/index.py
@@ -6,8 +6,8 @@ import seaborn as sns
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -16,15 +16,6 @@ import numpy as np
 
 # Load the Excel file
 dataset = pd.read_csv('../data_collecting/darwin_processes.csv')
-
-# Assigning couple columns variables to X
-X = dataset.iloc[:, 3:-4]
-# Assigning the last 2 column variable to y
-y = dataset.iloc[:, -4:]
-print(dataset.columns)
-print(dataset.dtypes)
-# print(X)
-# print(y)
 
 objTypeCols = dataset[[
     i for i in dataset.columns if dataset[i].dtype == 'object']]
@@ -35,66 +26,87 @@ corrprocessData = dataset.corr()
 
 # Dropping un-necessary information
 
-# Model building
-# test_size: 25% of the data will go to the test set, whereas the remaining 75% to the training set
-# random_state: using this parameter makes sure that anyone who re-runs your code will get the exact same outputs.
-# Popular integer random seeds are 0 and 42.
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.25, random_state=42)
-# print(X_train)
-# print(X_test)
-# print(y_train)
-# print(y_test)
+# Label Encoding
+# le = LabelEncoder() # LabelEncoder - Code categories into 0,1,2.....
+# for i in objTypeCols.columns.values.tolist()[1:]:
+#     dataset[i+"_coded"] = dataset[i].astype('category')
+#     dataset[i+"_coded"] = le.fit_transform(dataset[i+"_coded"])
 
-######################################### Linear Regression ###############################################
-lr = LinearRegression()
+# One-Hot Encoding
+# for i in objTypeCols.columns.values.tolist()[1:]:
+#     dummyCols = pd.get_dummies(dataset[i], prefix=i) # Convert 1/0 based on presence; C no. of columns
+#     dataset = dataset.join(dummyCols)
+# ohe = OneHotEncoder()
 
-lr.fit(X_train, y_train)
+# Assigning couple columns variables to X
+X = dataset.iloc[:, 3:-4]
+# Assigning the last 2 column variable to y
+y = dataset.iloc[:, -4:]
+print(dataset.columns)
+print(dataset.dtypes)
+# print(X)
+# print(y)
 
-y_lr_train_pred = lr.predict(X_train)
-y_lr_test_pred = lr.predict(X_test)
+# # Model building
+# # test_size: 25% of the data will go to the test set, whereas the remaining 75% to the training set
+# # random_state: using this parameter makes sure that anyone who re-runs your code will get the exact same outputs.
+# # Popular integer random seeds are 0 and 42.
+# X_train, X_test, y_train, y_test = train_test_split(
+#     X, y, test_size=0.25, random_state=42)
+# # print(X_train)
+# # print(X_test)
+# # print(y_train)
+# # print(y_test)
 
-# Model performance
-# variables contain the performance metrics MSE and R2 for models build using linear regression on the training set
-# MSE: the squared distance between actual and predicted values. Squared to avoid the cancellation of negative terms.
-lr_train_mse = mean_squared_error(y_train, y_lr_train_pred)
-lr_train_r2 = r2_score(y_train, y_lr_train_pred)
+# ######################################### Linear Regression ###############################################
+# lr = LinearRegression()
 
-lr_test_mse = mean_squared_error(y_test, y_lr_test_pred)
-lr_test_r2 = r2_score(y_test, y_lr_test_pred)
+# lr.fit(X_train, y_train)
 
-lr_results = pd.DataFrame(['Linear regression', lr_train_mse,
-                          lr_train_r2, lr_test_mse, lr_test_r2]).transpose()
-lr_results.columns = ['Method', 'Training MSE',
-                      'Training R2', 'Test MSE', 'Test R2']
-print(lr_results)
-print("Test R2 / Accuracy:", lr_test_r2*100, '%')
-print("Test RMSE: ", np.sqrt((lr_test_mse)))
+# y_lr_train_pred = lr.predict(X_train)
+# y_lr_test_pred = lr.predict(X_test)
 
-######################################### Random Forest ###############################################
-# build a random forest with 1000 decision trees
-rr = RandomForestRegressor(n_estimators=1000, random_state=42)
+# # Model performance
+# # variables contain the performance metrics MSE and R2 for models build using linear regression on the training set
+# # MSE: the squared distance between actual and predicted values. Squared to avoid the cancellation of negative terms.
+# lr_train_mse = mean_squared_error(y_train, y_lr_train_pred)
+# lr_train_r2 = r2_score(y_train, y_lr_train_pred)
 
-rr.fit(X_train, y_train)
+# lr_test_mse = mean_squared_error(y_test, y_lr_test_pred)
+# lr_test_r2 = r2_score(y_test, y_lr_test_pred)
 
-y_rr_train_pred = rr.predict(X_train)
-y_rr_test_pred = rr.predict(X_test)
+# lr_results = pd.DataFrame(['Linear regression', lr_train_mse,
+#                           lr_train_r2, lr_test_mse, lr_test_r2]).transpose()
+# lr_results.columns = ['Method', 'Training MSE',
+#                       'Training R2', 'Test MSE', 'Test R2']
+# print(lr_results)
+# print("Test R2 / Accuracy:", lr_test_r2*100, '%')
+# print("Test RMSE: ", np.sqrt((lr_test_mse)))
 
-# Model performance
-# variables contain the performance metrics MSE and R2 for models build using random forest on the training set
-rr_train_mse = mean_squared_error(y_train, y_rr_train_pred)
-rr_train_r2 = r2_score(y_train, y_rr_train_pred)
+# ######################################### Random Forest ###############################################
+# # build a random forest with 1000 decision trees
+# rr = RandomForestRegressor(n_estimators=1000, random_state=42)
 
-rr_test_mse = mean_squared_error(y_test, y_rr_test_pred)
-rr_test_r2 = r2_score(y_test, y_rr_test_pred)
+# rr.fit(X_train, y_train)
 
-rr_results = pd.DataFrame(['Random forest', rr_train_mse,
-                          rr_train_r2, rr_test_mse, rr_test_r2]).transpose()
-rr_results.columns = ['Method', 'Training MSE',
-                      'Training R2', 'Test MSE', 'Test R2']
-print(rr_results)
-print("Test R2 / Accuracy:", rr_test_r2*100, '%')
-print("Test RMSE: ", np.sqrt((rr_test_mse)))
+# y_rr_train_pred = rr.predict(X_train)
+# y_rr_test_pred = rr.predict(X_test)
+
+# # Model performance
+# # variables contain the performance metrics MSE and R2 for models build using random forest on the training set
+# rr_train_mse = mean_squared_error(y_train, y_rr_train_pred)
+# rr_train_r2 = r2_score(y_train, y_rr_train_pred)
+
+# rr_test_mse = mean_squared_error(y_test, y_rr_test_pred)
+# rr_test_r2 = r2_score(y_test, y_rr_test_pred)
+
+# rr_results = pd.DataFrame(['Random forest', rr_train_mse,
+#                           rr_train_r2, rr_test_mse, rr_test_r2]).transpose()
+# rr_results.columns = ['Method', 'Training MSE',
+#                       'Training R2', 'Test MSE', 'Test R2']
+# print(rr_results)
+# print("Test R2 / Accuracy:", rr_test_r2*100, '%')
+# print("Test RMSE: ", np.sqrt((rr_test_mse)))
 
 
 # Data visualization of prediction results
