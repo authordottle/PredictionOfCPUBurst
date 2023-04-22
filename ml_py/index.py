42893ed67fa2f7b186baad2e4fcc6743659efaf6
Jachian <author.tle@gmail.com>
2023-04-22 00:22:33 -0400

    modify

--- ml_py/index.py
+++ ml_py/index.py
@@ -1,6 +1,6 @@
 # /********* index.py ***********/
 
-# Import library
+""" Import library """
 from scipy import stats
 import scipy as sp
 import pandas as pd  # Data manipulation
@@ -24,39 +24,33 @@ import statsmodels.api as sm
 
 # print('version: {}'.format(pd.__version__))
 
-# Ask user for input
+""" Ask user for input """
 input = input(
     "Enter the platform data to start (enter either `linux` or `macBook`): ")
 
-# Load the csv file
+""" Load the csv file """
 if (input == "macBook"):
     # pid,name,username,memory_percent,cpu_percent,nice,create_time,mem_rss,mem_vms,mem_pfaults,mem_pageins,num_ctx_switches_voluntary,num_ctx_switches_involuntary,utime,stime,cutime,cstime
     df = pd.read_csv('../data_collecting/darwin_processes.csv')
-    X_drop_columns = ['stime']
-    multicollinearity_drop_columns = [
-        "memory_percent", "num_ctx_switches_voluntary"]
-    y_column = 'stime'
+    X_drop_columns = ['mem_pfaults']
+    multicollinearity_drop_columns = []
+    y_column = 'mem_pfaults'
 elif (input == "linux"):
     # PID, NAME, ELAPSED_TIME, TOTAL_TIME, utime, stime, start_time, uptime
     df = pd.read_csv('../data_collecting/linux_log_file.csv')
     X_drop_columns = ['stime']
+    # TODO: Manually determined here
     multicollinearity_drop_columns = [
         "ELAPSED_TIME", "TOTAL_TIME", "uptime"]
     y_column = 'stime'
 else:
     print("Invalid input")
 
-# Check for df
+""" Check df """
 # print(df.columns)
 # print(df.dtypes)
 
-# Check one column vs another plot
-# sns.lmplot(x='utime', y='stime', data=df, aspect=2, height=6)
-# plt.xlabel('utime: as Independent variable')
-# plt.ylabel('stime: as Dependent variable')
-# plt.title('utime Vs stime')
-# plt.show()
-
+""" Get obj cols and obj-col df """
 obj_type_cols = [
     i for i in df.columns if df[i].dtype == 'object']
 # Filter obj type columns
@@ -68,92 +62,74 @@ elif (input == "linux"):
 df_obj_type_cols = df[obj_type_cols]
 # print(df_obj_type_cols)
 
-# Find the correlation among the columns
+""" Find the correlation among the columns """
 corr = df.corr()
 # print(corr)
 
-# Show correlation plot
+""" Show correlation plot """
 # plt.figure(figsize=(12, 4))
 # sns.heatmap(corr, cmap='Wistia', annot=True)
 # plt.title('Correlation in the dataset')
 # plt.show()
 
-# Drop un-necessary information such as all 0 in the same col
-for col in df.columns:
-    if (df[col] == 0).all(axis=0):
-        multicollinearity_drop_columns.append(col)
-# print(multicollinearity_drop_columns)
-
-necessary_data_df = df.drop(multicollinearity_drop_columns, axis=1)
-# print(df.head())
-
-# ML algorithms cannot work with categorical data directly, categorical data must be converted to number.
+""" ML algorithms cannot work with categorical data directly, categorical data must be converted to number.
 # 1. Label Encoding
 # 2. One hot encoding
 # 3. Dummy variable trap
+"""
 
 # 1. Label Encoding
 le = LabelEncoder()  # Code categories into 0,1,2.....
-df_encode = necessary_data_df
+updated_df = df
 for col in df_obj_type_cols:
-    df_encode[col] = le.fit_transform(df_encode[col])
-# print(df_encode.head())
+    updated_df[col] = le.fit_transform(updated_df[col])
+# print(updated_df.head())
 
-# 2. One-Hot Encoding
+# 2. One-Hot Encoding (Not best work in this case)
 # ohe = OneHotEncoder()
-# df_encode = necessary_data_df
 # for col in df_obj_type_cols:
 #     # Reshape the 1-D array to 2-D as fit_transform expects 2-D and finally fit the object
 #     ohe_data = ohe.fit_transform(
-#         df_encode[col].values.reshape(-1, 1)).toarray()
+#         updated_df[col].values.reshape(-1, 1)).toarray()
 #     # Add this back into the original dataframe
 #     ohe_df = pd.DataFrame(
 #         ohe_data, columns=[col+"_"+str(int(i)) for i in range(ohe_data.shape[1])])
-#     df_encode = pd.concat([df_encode, ohe_df], axis=1)
+#     updated_df = pd.concat([updated_df, ohe_df], axis=1)
 # # Drop the obj columns
-# df_encode = df_encode.drop(df_obj_type_cols, axis=1)
-# # Verify
-# print(df_encode.head())
+# updated_df = updated_df.drop(df_obj_type_cols, axis=1)
 
-# 3. Dummy variable trap
-# df_encode = pd.get_dummies(data=necessary_data_df, prefix='OHE', prefix_sep='_',
+# 3. Dummy variable trap (Not best work in this case)
+# updated_df = pd.get_dummies(data=updated_df, prefix='OHE', prefix_sep='_',
 #                            columns=obj_type_cols,
 #                            drop_first=True,
 #                            dtype='int8')
-# # Verify the dummay variable process
-# print('Columns in original data frame:', necessary_data_df.columns.values)
-# print('Number of rows and columns in the dataset:', necessary_data_df.shape)
-# print('Columns in data frame after encoding dummy variable: ',
-#       df_encode.columns.values)
-# print('Number of rows and columns in the dataset: ', df_encode.shape)
 
-# Remove all rows that have outliers in, at least, one column
+""" Drop un-necessary information such as all 0 in the same col """
+for col in updated_df.columns:
+    if (updated_df[col] == 0).all(axis=0):
+        updated_df = updated_df.drop(col, axis=1)
+# print(updated_df)
+
+""" Remove all rows that have outliers in, at least, one column """
 # For each column, it first computes the Z-score of each value in the column, relative to the column mean and standard deviation.
 # It then takes the absolute Z-score because the direction does not matter, only if it is below the threshold.
 # "all(axis=1)" ensures that for each row, all column satisfy the constraint.
 # Finally, the result of this condition is used to index the dataframe.
-no_outliers_df = df_encode[(np.abs(stats.zscore(df_encode)) < 3).all(axis=1)]
+updated_df = updated_df[(
+    np.abs(stats.zscore(updated_df)) < 3).all(axis=1)]
 
-# Check for missing value
+""" Check missing value """
 # plt.figure(figsize=(12, 4))
-# sns.heatmap(no_outliers_df.isnull(), cbar=False, cmap='viridis', yticklabels=False)
+# sns.heatmap(updated_df.isnull(), cbar=False, cmap='viridis', yticklabels=False)
 # plt.title('Missing value in the dataset')
 # plt.show()
 
-# Finish data preprocessing
-preprocessed_df = no_outliers_df
-print(preprocessed_df)
+""" Finish data preprocessing """
+# print(df)
+# print(updated_df)
 
-# Assign columns variables to X and y
-X_df = preprocessed_df.drop(X_drop_columns, axis=1)  # Independent variable
-
-# utime: CPU time spent in user code
-# stime: CPU time spent in kernel code
-y_df = preprocessed_df[y_column]  # Dependent variable
-
-#
-X = X_df.values
-y = y_df.values
+######################################### Linear Regression ###############################################
+updated_lr_df = updated_df
 
 
 def calculate_vif(data):
@@ -168,17 +144,40 @@ def calculate_vif(data):
         # print(r_squared)
         if r_squared == 1:
             print(x_var_names[i] + " r-squared is 1 which causes infinity.")
-            exit()
-        vif = round(1/(1-r_squared), 2)
-        vif_df.loc[i] = [x_var_names[i], vif]
+            multicollinearity_drop_columns.append(x_var_names[i])
+        else:
+            vif = round(1/(1-r_squared), 2)
+            if vif > 5:
+                multicollinearity_drop_columns.append(x_var_names[i])
+            vif_df.loc[i] = [x_var_names[i], vif]
     return vif_df.sort_values(by='Vif', axis=0, ascending=False, inplace=False)
 
 
-# V) Calculate VIF to verify multicollinearity for LR
-vif_df = calculate_vif(X_df)
-print(vif_df)
+""" V) Calculate VIF to verify multicollinearity for LR """
+if multicollinearity_drop_columns == []:
+    vif_df = calculate_vif(updated_lr_df)
+    updated_lr_df = updated_lr_df.drop(multicollinearity_drop_columns, axis=1)
+else:
+    updated_lr_df = updated_lr_df.drop(multicollinearity_drop_columns, axis=1)
+    multicollinearity_drop_columns = []
+    vif_df = calculate_vif(updated_lr_df)
+    updated_lr_df = updated_lr_df.drop(multicollinearity_drop_columns, axis=1)
+# print(multicollinearity_drop_columns)
+# print(vif_df)
+# print(updated_lr_df)
 
-# Train test split
+""" Assign columns variables to X and y """
+X_df = updated_lr_df.drop(X_drop_columns, axis=1)  # Independent variable
+
+# utime: CPU time spent in user code
+# stime: CPU time spent in kernel code
+y_df = updated_lr_df[y_column]  # Dependent variable
+
+""""""
+X = X_df.values
+y = y_df.values
+
+""" Train test split """
 # test_size: 25% of the data will go to the test set, whereas the remaining 75% to the training set
 # random_state: using this parameter makes sure that anyone who re-runs your code will get the exact same outputs.
 # Popular integer random seeds are 0 and 42.
@@ -191,6 +190,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 # print(y_train)
 # print(y_test)
 
+""""""
 sc = StandardScaler()
 X_train_transform = sc.fit_transform(X_train)
 X_test_transform = sc.fit_transform(X_test)
@@ -221,41 +221,37 @@ def get_theta_by_gradient_descent(X, y, learning_rate, iterations):
     return theta
 
 
-# Find the multivarite regression model for the data set
+""" Find the multivarite regression model for the data set  """
 # Parameters: X: independent variables matrix; y: dependent variables matrix; learning_rate: learning rate of Gradient Descent; iterations: the number of iterations
 # Return: the final theta vector and the plot of cost function
 mlr_theta = get_theta_by_gradient_descent(
     X_train_transform, y_train.tolist(), 0.03, 30000)
 
-######################################### Linear Regression ###############################################
-# I. Training a Linear Regression model which is ordinary least squares Linear Regression by scikit-learn implementation
+""" I. Training a Linear Regression model which is ordinary least squares Linear Regression by scikit-learn implementation """
 lr = LinearRegression()
 lr.fit(X_train_transform, y_train)
 
-# Check they match or not
+""" Check they match or not """
 flat_mlr_theta_list = [item for sublist in mlr_theta for item in sublist]
 sub_list = [lr.intercept_]
 lr_info_list = [*sub_list, *(lr.coef_)]
 # print(flat_mlr_theta_list)
 # print(lr_info_list)
 
-# Predict by sklearn linear regression model
+""" Predict by sklearn linear regression model """
 y_lr_train_pred = lr.predict(X_train_transform)
 y_lr_test_pred = lr.predict(X_test_transform)
 # print(y_lr_train_pred)
 # print(y_lr_test_pred)
 
-# Evaluation: MSE
-# variables contain the performance metrics MSE and R2 for models build using linear regression on the training set
-# MSE: the squared distance between actual and predicted values. Squared to avoid the cancellation of negative terms.
+######################################### Evaluation ###############################################
+""" MSE and R2 """
 lr_train_mse = mean_squared_error(y_train, y_lr_train_pred)
-# or lr_train_r2 = r2_score(y_train, y_lr_train_pred)
 lr_train_r2 = lr.score(X_train_transform, y_train)
 lr_test_mse = mean_squared_error(y_test, y_lr_test_pred)
-# or lr_test_r2 = r2_score(y_test, y_lr_test_pred)
 lr_test_r2 = lr.score(X_test_transform, y_test)
 
-# Compute the cross-validation scores
+""" Compute the cross-validation scores """
 # n_splits, default=5; n_repeats, default=10
 cv = RepeatedKFold(random_state=42)
 lr_mse_scores = cross_val_score(
@@ -267,31 +263,31 @@ lr_std_scores = lr_mse_scores.std()
 lr_rmse_mean_scores = abs(lr_rmse_scores.mean())
 lr_rmse_std_scores = lr_rmse_scores.std()
 
-# Validation
-# I) Check for Linearity
+######################################### Validation ###############################################
+""" I) Check for Linearity """
 f = plt.figure(figsize=(14, 5))
 ax1 = f.add_subplot(121)
 sns.scatterplot(x=y_test, y=y_lr_test_pred, ax=ax1, color='r')
 ax1.set_title('Check for Linearity:\n Actual Vs Predicted value')
 
-# II) Check for Residual normality & mean
+""" II) Check for Residual normality & mean """
 ax2 = f.add_subplot(122)
 sns.histplot(data=(y_test - y_lr_test_pred), ax=ax2, color='b')
 ax2.axvline((y_test - y_lr_test_pred).mean(), color='yellow', linestyle='--')
 ax2.set_title('Check for Residual normality & mean: \n Residual eror')
 
-# III) Check for Multivariate Normality by Quantile-Quantile plot
+""" III) Check for Multivariate Normality by Quantile-Quantile plot """
 f, ax = plt.subplots(1, 2, figsize=(14, 6))
 _, (_, _, r) = sp.stats.probplot(
     (y_test - y_lr_test_pred), fit=True, plot=ax[0])
 ax[0].set_title('Check for Multivariate Normality: \nQ-Q Plot')
 
-# IV) Check for Homoscedasticity
+""" IV) Check for Homoscedasticity """
 sns.scatterplot(y=(y_test - y_lr_test_pred),
                 x=y_lr_test_pred, ax=ax[1], color='r')
 ax[1].set_title('Check for Homoscedasticity: \nResidual Vs Predicted')
 
-# Data visualization of prediction results for lr
+######################################### Visualization ###############################################
 plt.figure(figsize=(5, 5))
 plt.scatter(x=y_train, y=y_lr_train_pred, c="#7CAE00", alpha=0.3)
 # A trend line to the plot
@@ -306,25 +302,49 @@ plt.xlabel('Experimental')
 # plt.show()
 
 ######################################### Random Forest ###############################################
-# build a random forest with 1000 decision trees
+updated_rfr_df = updated_df
+
+""" Assign columns variables to X and y """
+X_df = updated_rfr_df.drop(X_drop_columns, axis=1)  # Independent variable
+
+# utime: CPU time spent in user code
+# stime: CPU time spent in kernel code
+y_df = updated_rfr_df[y_column]  # Dependent variable
+
+""""""
+X = X_df.values
+y = y_df.values
+
+""" Train test split """
+# test_size: 25% of the data will go to the test set, whereas the remaining 75% to the training set
+# random_state: using this parameter makes sure that anyone who re-runs your code will get the exact same outputs.
+# Popular integer random seeds are 0 and 42.
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.25, random_state=42)
+# print(X_train)
+# print(X_train.dtypes)
+# print(X_train.shape)
+# print(X_test)
+# print(y_train)
+# print(y_test)
+
+""" Build a random forest with 1000 decision trees """
 rfr = RandomForestRegressor(n_estimators=1000, random_state=42)
-rfr.fit(X_train_transform, y_train)
-y_rfr_train_pred = rfr.predict(X_train_transform)
-y_rfr_test_pred = rfr.predict(X_test_transform)
+rfr.fit(X_train, y_train)
+y_rfr_train_pred = rfr.predict(X_train)
+y_rfr_test_pred = rfr.predict(X_test)
 # print(y_train)
 # print(y_rfr_train_pred)
 # print(y_rfr_train_pred)
 
-# Evaluation: MSE
-# variables contain the performance metrics MSE and R2 for models build using random forest on the training set
+######################################### Evaluation ###############################################
+""" MSE and R2 """
 rfr_train_mse = mean_squared_error(y_train, y_rfr_train_pred)
-# or rfr_train_r2 = r2_score(y_train, y_rfr_train_pred)
-rfr_train_r2 = rfr.score(X_train_transform, y_train)
+rfr_train_r2 = rfr.score(X_train, y_train)
 rfr_test_mse = mean_squared_error(y_test, y_rfr_test_pred)
-# or rfr_test_r2 = r2_score(y_test, y_rfr_test_pred)
-rfr_test_r2 = rfr.score(X_test_transform, y_test)
+rfr_test_r2 = rfr.score(X_test, y_test)
 
-# Compute the cross-validation scores
+""" Compute the cross-validation scores """
 # n_splits, default=5; n_repeats, default=10
 cv = RepeatedKFold(random_state=42)
 rfr_scores = cross_val_score(
@@ -336,7 +356,7 @@ rfr_std_scores = rfr_scores.std()
 rfr_rmse_mean_scores = abs(rfr_rmse_scores.mean())
 rfr_rmse_std_scores = rfr_rmse_scores.std()
 
-# Data visualization of prediction results for rfr
+######################################### Visualization ###############################################
 plt.figure(figsize=(5, 5))
 plt.scatter(x=y_train, y=y_rfr_train_pred, c="#7CAE00", alpha=0.3)
 # A trend line to the plot
@@ -351,13 +371,16 @@ plt.show()
 
 ######################################### Conclusion of Evaluation ###############################################
 data = [
-    ['Linear regression', lr_train_mse, np.sqrt((lr_train_mse)),
-     lr_train_r2, lr_test_mse, np.sqrt((lr_test_mse)), lr_test_r2, lr_mse_mean_scores, lr_rmse_mean_scores, lr_std_scores],
-    ['Random forest', rfr_train_mse, np.sqrt((rfr_train_mse)),
-     rfr_train_r2, rfr_test_mse, np.sqrt((rfr_test_mse)), rfr_test_r2, rfr_mse_mean_scores, rfr_rmse_mean_scores, rfr_std_scores],
+    ['Linear regression',
+     # lr_train_mse, np.sqrt((lr_train_mse)), lr_train_r2,
+     lr_test_mse, np.sqrt((lr_test_mse)), lr_test_r2, lr_mse_mean_scores, lr_rmse_mean_scores, lr_std_scores],
+    ['Random forest',
+     # rfr_train_mse, np.sqrt((rfr_train_mse)), rfr_train_r2,
+     rfr_test_mse, np.sqrt((rfr_test_mse)), rfr_test_r2, rfr_mse_mean_scores, rfr_rmse_mean_scores, rfr_std_scores],
 ]
 results = pd.DataFrame(data,
-                       columns=['Method', 'Training MSE', 'Training RMSE',
-                                'Training R2', 'Test MSE', 'Test RMSE', 'Test R2', 'Cross-Val MSE Mean', 'Cross-Val RMSE Mean', 'Cross-Val MSE Standard Deviation']
+                       columns=['Method',
+                                # 'Training MSE', 'Training RMSE', 'Training R2',
+                                'Test MSE', 'Test RMSE', 'Test R2', 'Cross-Val MSE Mean', 'Cross-Val RMSE Mean', 'Cross-Val MSE Standard Deviation']
                        )
 print(results)
